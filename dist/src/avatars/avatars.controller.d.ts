3b1f1a8fd1b4afc2841fd9dc8b0ae64daeb0869c
boostfek-droid <boostfek@gmail.com>
2026-04-10 09:55:46 +0100

    fix: remove dist from git tracking to prevent vcg-linux architecture mismatch

--- dist/src/avatars/avatars.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { AvatarsService } from './avatars.service';
-export declare class AvatarsController {
-    private readonly avatarsService;
-    constructor(avatarsService: AvatarsService);
-    uploadFile(file: any): Promise<any>;
-}
--- dist/src/main.d.ts
+++
@@ -1,3 +0,0 @@
-import 'reflect-metadata';
-declare const _default: (req: any, res: any) => Promise<any>;
-export default _default;
